ffbdf1a065a929a3a00eea27e951023fc4d68d89
Josh Chorlton <jchorlton@gmail.com>
2020-04-05 22:13:25 -0700

    taking inputs and excludes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
-use clap::{value_t, App, Arg};
+use clap::{value_t, App, Arg, Values};
 
 fn main() {
     let matches = App::new("borgman")
         .version("0.0.1")
         .author("josh chorlton")
         .about("Manages the borg (https://www.borgbackup.org/)")
+        .arg(
+            Arg::with_name("INPUTS")
+                .help("paths to archive")
+                .required(true)
+                .multiple(true),
+        )
+        .arg(
+            Arg::with_name("EXCLUDES")
+                .help("exclude paths matching PATTERN")
+                .takes_value(true)
+                .short('e')
+                .long("exclude")
+                .multiple(true),
+        )
         .arg(
             Arg::with_name("KEEP_DAILY")
                 .short('d')
@@ -34,6 +48,14 @@ fn main() {
         )
         .get_matches();
 
+    let inputs = matches
+        .values_of("INPUTS")
+        .unwrap_or(Values::default())
+        .collect::<String>();
+    let excludes = matches
+        .values_of("EXCLUDES")
+        .unwrap_or(Values::default())
+        .collect::<String>();
     let keep_daily = value_t!(matches, "KEEP_DAILY", u8).unwrap();
     let keep_weekly = value_t!(matches, "KEEP_WEEKLY", u8).unwrap();
     let keep_monthly = value_t!(matches, "KEEP_MONTHLY", u8).unwrap();
